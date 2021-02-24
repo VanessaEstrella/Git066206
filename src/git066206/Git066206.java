ccf373b8a986b3b0fdc6c584913ac478a7a9a5e1
starco <starco@192.168.10.217>
2021-02-24 10:15:05 -0600

    mi primer commit

---
+++ src/git066206/Git066206.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git066206;
+
+/**
+ *
+ * @author starco
+ */
+public class Git066206 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String nombre = "vanessa ";
+        int edad = 18;
+        
+        System.out.println("soy " + nombre + " y tengo " + edad);
+        
+    }
+    
+}
